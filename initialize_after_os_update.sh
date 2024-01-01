8b18a0e3e5d00bef9c1f2f8fecfead582d920d6a
Florian Kempenich <Flori@nKempenich.com>
2024-01-01 18:08:59 +0000

    Fix init script for Steam OS 3.5+

--- initialize_after_os_update.sh
+++ initialize_after_os_update.sh
@@ -7,20 +7,37 @@ cd "$DIR"
 # Disable SteamOS Readonly mode
 sudo steamos-readonly disable
 
+# If the '--skip-refresh-keys' flag is passed, skip refreshing keys
+if [[ "$*" != *--skip-refresh-keys* ]]; then
+    # Refresh Keys
+    sudo pacman-key --init
+    sudo pacman-key --populate archlinux 
+    sudo pacman-key --populate holo # Thx: https://www.reddit.com/r/SteamDeck/comments/17i7gdk/comment/k6smppj
+    sudo pacman-key --refresh-keys
+fi
+
+# Tell user to press enter to continue
+
+echo ""
+echo ""
+echo "------------------------------"
+echo "| Press Enter to continue... |"
+echo "------------------------------"
+read
+echo ""
+exit 0
+
 # Update archlinux-keyring
 # See: https://wiki.archlinux.org/title/Pacman/Package_signing#Upgrade_system_regularly
 sudo pacman -Sy archlinux-keyring --noconfirm
 sudo pacman -Su --noconfirm
-
-# Refresh Keys
-sudo pacman-key --init
-sudo pacman-key --populate archlinux 
-sudo pacman-key --refresh-keys
 
 # Upgrade all packages
 sudo pacman -Syu --noconfirm
 
 # Install custom packages
 ./install_pacman_packages.sh
+
+echo "Done!"
 
 cd - >/dev/null
